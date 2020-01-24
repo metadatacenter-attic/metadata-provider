7547828255fb666f41691a1e46e2eb3c2aecc17b
Marcos Martinez <marcos.martinez.romero@gmail.com>
2020-01-24 14:43:48 -0800

    Export samples to JSON

---
+++ scripts/step9_save_to_mongo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import scripts.constants as constants
+import scripts.util.export_utils as export_utils
+
+INPUT_FILE = constants.NCBI_EXPORT_INPUT_FILE
+OUTPUT_FILE = constants.NCBI_EXPORT_JSON_OUTPUT_FILE
+EXPORT_FILTER_SPECS = constants.NCBI_EXPORT_FILTER_SPECS
+
+
+def main():
+
+    export_utils.export_samples_to_json(constants.ROOT_FOLDER_NAME, INPUT_FILE, OUTPUT_FILE, 10000)
+
+
+if __name__ == "__main__":
+    main()
