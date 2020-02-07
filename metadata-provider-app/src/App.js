7850dc0aaba30614dad04bc7516911b67c42345b
Marcos Martinez <marcos.martinez.romero@gmail.com>
2020-02-07 12:51:26 -0800

    Populate search field when clicking on sample queries

--- metadata-provider-app/src/App.js
+++ metadata-provider-app/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button, Form} from 'react-bootstrap';
-import {useState} from "react";
+import Table from "react-bootstrap/Table";
 
 
 function App() {
@@ -12,7 +12,20 @@ function App() {
     const [showResults, setShowResults] = useState(false);
     const [searchQuery, setSearchQuery] = useState("");
     const [showEnterQueryMessage, setShowEnterQueryMessage] = useState(false);
-    
+
+    const sampleQueriesOriginal = [
+        "disease=liver cancer AND tissue=liver",
+        "disease=hepatoma AND tissue=liver",
+        "disease=hepatocellular carcinoma AND tissue=liver",
+        "disease=hepatocellular carcinoma AND tissue=cancerous liver tissue"
+    ];
+
+  const sampleQueriesAnnotated = [
+    "NCIT:C2991=NCIT:C7927 AND NCIT:C12801=NCIT:C12392"
+  ];
+
+
+
     function queryOriginalSamples(e) {
         e.preventDefault();
         fetchData("original")
@@ -42,10 +55,14 @@ function App() {
         }
     }
 
+    function updateSearchQuery(e, v) {
+      setSearchQuery(v);
+    }
+
     return (
         <div className="App">
             <div className="App-content">
-                <h2>Metadata Provider</h2>
+                <h1>Metadata Provider</h1>
                 <Form className="mt-4">
                     <Form.Group controlId="formSearch">
                         <Form.Control
@@ -58,36 +75,66 @@ function App() {
                                 setSearchQuery(e.target.value)
                             }}
                         />
-                        <Form.Text className="text-muted">
-                            Examples: <br/>disease=liver cancer AND tissue=liver <br/>
-                            NCIT:C2991=NCIT:C7927 AND NCIT:C12801=NCIT:C12392
-                        </Form.Text>
+                        <div className="examples">
+                          <h5>Examples:</h5>
+                          <h6>Original samples</h6>
+                          <ul>
+                          {sampleQueriesOriginal.map((item, index) => (
+                              <li key={index} onClick={e => updateSearchQuery(e, sampleQueriesOriginal[index])}>{item}</li>
+                          ))}
+                          </ul>
+
+                          <h6>Curated samples</h6>
+                          <ul>
+                            {sampleQueriesAnnotated.map((item, index) => (
+                                <li key={index} onClick={e => updateSearchQuery(e, sampleQueriesAnnotated[index])}>{item}</li>
+                            ))}
+                          </ul>
+                        </div>
                     </Form.Group>
 
-                    <Button id="original-button" variant="primary" type="submit"
+                    <Button id="original-button" variant="info" type="submit"
                             onClick={e => {
-                              queryOriginalSamples(e)
+                                queryOriginalSamples(e)
                             }}>
                         Search samples
                     </Button>
                     <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                    <Button id="annotated-button" variant="primary" type="submit"
+                    <Button id="annotated-button" variant="info" type="submit"
                             onClick={e => {
-                              queryAnnotatedSamples(e)
+                                queryAnnotatedSamples(e)
                             }}>
                         Search curated samples
                     </Button>
                 </Form>
                 {showResults &&
                 <div className="results">
+
                     <p>Number of samples found: {samples.length}</p>
-                    <ol>
-                        {samples.map(item => (
-                            <li key={item.biosampleAccession}>
-                                <a href={item.biosampleUrl}>{item.biosampleAccession}</a>
-                            </li>
+
+                    <Table striped bordered hover variant="dark">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>BioSample ID</th>
+                            <th>Name</th>
+                            {/*<th>BioProject ID</th>*/}
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {samples.map((item, index) => (
+                            <tr>
+                                <td>{index + 1}</td>
+                                <td><a href={item.biosampleUrl}>{item.biosampleAccession}</a></td>
+                                <td>{item.sampleName ? item.sampleName : 'NA'}</td>
+                                {/*<td>{item.bioprojectAccession}</td>*/}
+                            </tr>
+
+
                         ))}
-                    </ol>
+                        </tbody>
+                    </Table>
+
                 </div>
                 }
                 {showEnterQueryMessage && <p>Enter a search query</p>}
