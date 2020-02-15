ab82b8a569c03b3331c33bf391e51b540d072056
Marcos Martinez <marcos.martinez.romero@gmail.com>
2020-02-15 08:55:02 -0800

    Sort samples by id

--- metadata-provider-api/src/main/java/org/metadatacenter/db/BiosampleService.java
+++ metadata-provider-api/src/main/java/org/metadatacenter/db/BiosampleService.java
@@ -8,7 +8,6 @@ import com.mongodb.client.MongoCursor;
 import org.bson.BsonDocument;
 import org.bson.Document;
 import org.bson.conversions.Bson;
-import org.metadatacenter.MetadataProviderApiConfiguration;
 import org.metadatacenter.api.Biosample;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +17,8 @@ import java.util.List;
 import java.util.Map;
 
 import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Sorts.ascending;
+import static com.mongodb.client.model.Sorts.orderBy;
 
 public class BiosampleService {
 
@@ -27,13 +28,14 @@ public class BiosampleService {
   private final boolean isAnnotatedSamplesCollection;
   private final ObjectMapper mapper = new ObjectMapper();
 
+  private final String SAMPLE_ID_FIELD = "biosampleAccession";
+  // Initialized in constructor
   private final String ATTRIBUTE_NAME_FIELD;
   private final String ATTRIBUTE_VALUE_FIELD;
 
   public BiosampleService(MongoCollection<Document> samplesCollection, boolean isAnnotatedSamplesCollection) {
     this.samplesCollection = samplesCollection;
     this.isAnnotatedSamplesCollection = isAnnotatedSamplesCollection;
-
     final String ATTRIBUTE_NAME_FIELD_ORIGINAL = "attributeName";
     final String ATTRIBUTE_VALUE_FIELD_ORIGINAL = "attributeValue";
     final String ATTRIBUTE_NAME_FIELD_ANNOTATED = "attributeNameTermUri";
@@ -79,7 +81,7 @@ public class BiosampleService {
     BsonDocument bsonDocument = searchFilter.toBsonDocument(BsonDocument.class,
         MongoClientSettings.getDefaultCodecRegistry());
     logger.info("Search filter: " + bsonDocument.toJson());
-    MongoCursor<Document> iterator = samplesCollection.find(searchFilter).iterator();
+    MongoCursor<Document> iterator = samplesCollection.find(searchFilter).sort(orderBy(ascending(SAMPLE_ID_FIELD))).iterator();
 
     try {
       while (iterator.hasNext()) {
