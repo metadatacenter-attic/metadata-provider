ff3055d8b8c7d1f6f6c132661b93b3dd20b46c37
Marcos Martinez <marcos.martinez.romero@gmail.com>
2020-02-14 17:55:12 -0800

    Dynamic highlighting of samples/projects buttons

--- metadata-provider-app/src/components/SearchComponent.js
+++ metadata-provider-app/src/components/SearchComponent.js
@@ -2,19 +2,12 @@ import React, {useEffect, useState} from 'react';
 import {Button, Form} from "react-bootstrap";
 import Table from "react-bootstrap/Table";
 import Col from "react-bootstrap/Col";
-import DropdownButton from "react-bootstrap/DropdownButton";
 import Dropdown from "react-bootstrap/Dropdown";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-import ButtonGroup from "react-bootstrap/ButtonGroup";
 import InputGroup from "react-bootstrap/InputGroup";
-import FormControl from "react-bootstrap/FormControl";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faSearch} from '@fortawesome/free-solid-svg-icons'
-import {faChevronDown} from '@fortawesome/free-solid-svg-icons'
-import {faAngleDown} from '@fortawesome/free-solid-svg-icons'
-import {faExternalLinkSquareAlt} from '@fortawesome/free-solid-svg-icons'
-import SearchResultsModalComponent from "./SearchResultsModalComponent";
+import {faAngleDown, faSearch} from '@fortawesome/free-solid-svg-icons'
 
 
 function SearchComponent(props) {
@@ -25,6 +18,7 @@ function SearchComponent(props) {
   const [searchQuery, setSearchQuery] = useState('disease=liver cancer');
   const [sampleQueries, setSampleQueries] = useState([]);
   const [showEnterQueryMessage, setShowEnterQueryMessage] = useState(false);
+  const [showSamplesOrProjects, setShowSamplesOrProjects] = useState('samples');
 
   function querySamples(e, db) {
     e.preventDefault();
@@ -46,10 +40,6 @@ function SearchComponent(props) {
     }
   };
 
-  function syncTextareaHeight() {
-
-  }
-
   // Similar to componentDidMount and componentDidUpdate
   useEffect(() => {
     if (props.sampleQueries) {
@@ -61,6 +51,10 @@ function SearchComponent(props) {
 
   function updateSearchQuery(e, v) {
     setSearchQuery(v);
+  }
+
+  function showContent(e, content) {
+    setShowSamplesOrProjects(content);
   }
 
   return (
@@ -93,7 +87,7 @@ function SearchComponent(props) {
                 value={searchQuery}
                 onChange={e => {
                   setShowEnterQueryMessage(false);
-                  setSearchQuery(e.target.value)
+                  setSearchQuery(e.target.value);
                 }}/>
               <InputGroup.Append>
                 <Button className="search-btn" type="submit" onClick={e => {
@@ -104,33 +98,41 @@ function SearchComponent(props) {
           </Container>
         </Form.Group>
       </Form>
-      {showResults &&
-      <>
-        {/*<SearchResultsModalComponent samples={samples}/>*/}
 
+      {showEnterQueryMessage && <p>Enter a search query</p>}
+      {hasError && <p>Search query error</p>}
+
+      <>
+        {showResults &&
         <Container>
           <Row>
             <Col>
               <Container>
                 <Row>
                   <Col md={2}></Col>
-                  <Col md={4} className="results-count results-count-left">
-                    <Container>
+                  <Col md={4} className={showSamplesOrProjects === "samples" ?
+                    "results-count results-count-left results-count-selected" : "results-count results-count-left"}>
+                    <Container onClick={e => showContent(e,'samples')}>
                       <Row><Col className="title">Samples</Col></Row>
                       <Row><Col className="count-left">{samples.length}</Col></Row>
                     </Container>
                   </Col>
-                  <Col md={4} className="results-count results-count-right"> <Container>
-                    <Row><Col className="title">Projects</Col></Row>
-                    <Row><Col className="count-right">{samples.length}</Col></Row>
-                  </Container>
+                  <Col md={4} className={showSamplesOrProjects === "projects" ?
+                    "results-count results-count-right results-count-selected" : "results-count results-count-right"}>
+                    <Container onClick={e => showContent(e,'projects')}>
+                      <Row><Col className="title">Projects</Col></Row>
+                      <Row><Col className="count-right">{samples.length}</Col></Row>
+                    </Container>
                   </Col>
                   <Col md={2}></Col>
                 </Row>
               </Container>
             </Col>
           </Row>
-
+        </Container>
+        }
+        {showResults && showSamplesOrProjects === 'samples' &&
+        <Container>
           <Row>
             <Col>
               <Container>
@@ -160,14 +162,41 @@ function SearchComponent(props) {
 
             </Col>
           </Row>
-
         </Container>
+        }
+        {showResults && showSamplesOrProjects === 'projects' &&
+        <Container>
+          <Row>
+            <Col>
+              <Container>
+                <div className="results">
+                  <Table striped bordered hover variant="dark">
+                    <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Project ID</th>
+                      <th>Title</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {/*{samples.map((item, index) => (*/}
+                    {/*  <tr key={index}>*/}
+                    {/*    <td>{index + 1}</td>*/}
+                    {/*    <td><a href={item.biosampleUrl}>{item.biosampleAccession}</a></td>*/}
+                    {/*    <td>{item.sampleName ? item.sampleName : 'NA'}</td>*/}
+                    {/*    /!*<td>{item.bioprojectAccession}</td>*!/*/}
+                    {/*  </tr>*/}
+                    {/*))}*/}
+                    </tbody>
+                  </Table>
+                </div>
+              </Container>
 
-
+            </Col>
+          </Row>
+        </Container>
+        }
       </>
-      }
-      {showEnterQueryMessage && <p>Enter a search query</p>}
-      {hasError && <p>Search query error</p>}
     </>
   );
 
